6f6f9b0d0388ddac141dbff7acd2b425e6f27224
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2019-08-14 08:15:07 +0700

    add footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,17 @@
 import React from "react"
+import css from "./footer.module.scss"
 
 const Footer = () => {
-  return <div></div>
+  return (
+    <div className={css.footer}>
+      <div className={css.footerText}>
+        <p>Footer</p>
+      </div>
+      <div className={css.name}>
+        <p>@gregoriusjimmy</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
